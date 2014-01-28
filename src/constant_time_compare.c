51029172fa7d85047c3d93e63a3095e84d94fb68
Levi Gross <levi@levigross.com>
2014-01-28 12:44:18 -0500

    Added comments and optimizations
    Signed-off-by: Levi Gross <levi@levigross.com>

--- src/constant_time_compare.c
+++ src/constant_time_compare.c
@@ -14,25 +14,29 @@ static PyObject *secure_compare(PyObject *self, PyObject *args)
     PyObject *temp_string_two;
     volatile const unsigned char *left;
     volatile const unsigned char *right;
+    Py_ssize_t string_size;
 
+    /* How many arguments were provided to the function */
     if (PyTuple_Size(args) != 2)
     {
         PyErr_SetString(PyExc_ValueError, "You must only supply 2 arguments");
         return NULL;
     }
-
+    /* Can we parse them? */
     if (!PyArg_ParseTuple(args, "OO", &temp_string_one, &temp_string_two)) 
     {
         PyErr_SetString(PyExc_TypeError, "Unable To Parse Function Arguments");
         return NULL;
     }
 
+    /* Are they Python Strings */
     if (!(PyString_CheckExact(temp_string_one)) || !(PyString_CheckExact(temp_string_two))) 
     {
         PyErr_SetString(PyExc_TypeError, "You must supply 2 string objects");
         return NULL;   
     } 
 
+    /* Are they the same size? */
     if (PyString_Size(temp_string_one) != PyString_Size(temp_string_two)) 
     {
         Py_RETURN_FALSE;
@@ -42,8 +46,9 @@ static PyObject *secure_compare(PyObject *self, PyObject *args)
     left = (const volatile unsigned char *) PyString_AsString(temp_string_one);
     right = (const volatile unsigned char *) PyString_AsString(temp_string_two);
     volatile unsigned char sentinel = 0;
+    string_size = PyString_Size(temp_string_one);
 
-    for(int i = 0; i < PyString_Size(temp_string_one); i++) 
+    for(int i = 0; i < string_size; i++)
     {
         sentinel |= *left++ ^ *right++;
     }
