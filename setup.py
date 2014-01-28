7533ebac2fabeeb53ef4af279a8c0bad7126cc43
Levi Gross <levi@levigross.com>
2014-01-28 10:55:55 -0500

    Updated setup.py to reflect namespace
    Signed-off-by: Levi Gross <levi@levigross.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, Extension
 
-ctc = Extension('constant_time_compare.src',
+ctc = Extension('constant_time_compare',
                 sources=['src/constant_time_compare.c'], language='c')
 
-setup(name='Constant Time Compare',
-      version='1.0',
+setup(name='constant_time_compare',
+      version='1.2',
       description='This package includes a secure constant time comparison function written in C',
       author='Levi Gross',
       author_email='yriv@yrivtebff.pbz'.decode('rot13'),
-      url="http://www.levigross.com/",
+      url="https://github.com/levigross/constant_time_compare",
+      platforms=['any'],
       ext_modules=[ctc])
